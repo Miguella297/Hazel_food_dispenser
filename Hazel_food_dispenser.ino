3dd5f7e3287fcd7f75e9bbe804a02601b2615bdd
Mike <mikeivanov2010@gmail.com>
2026-08-21 11:43:21 -0400

    editd

--- Hazel_food_dispenser.ino
+++ Hazel_food_dispenser.ino
@@ -1,18 +1,15 @@
 #include <Servo.h>
-#include <EEPROM.h>
 
 Servo myServo;
 
 const int servoPin = 8;
-const int stepDelay = 5;      // ms between each 1-degree step (controls "slow" speed)
+const int stepDelay = 12;   // controls speed - lower = faster
 
 void setup() {
   myServo.attach(servoPin);
   myServo.write(0);
   delay(300);
-}
 
-void loop() {
   // rotate 0 -> 180
   for (int pos = 0; pos <= 180; pos++) {
     myServo.write(pos);
@@ -20,8 +17,12 @@ void loop() {
   }
 
   // rotate 180 -> 0
-  for (int pos = 180; pos >= 0; pos--) {
+  for (int pos = 180; pos >= 0; pos --) {
     myServo.write(pos);
     delay(stepDelay);
-  }
+  }  
+}
+
+void loop() {
+  // intentionally empty - nothing repeats
 }
