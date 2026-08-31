3d41f7a9fc098a633c177243a4a503c3ac10e8a9
Mike <mikeivanov2010@gmail.com>
2026-08-31 17:53:47 -0400

    final updated code once and for all (I hope)

--- Hazel_food_dispenser.ino
+++ Hazel_food_dispenser.ino
@@ -11,7 +11,7 @@ void setup() {
   delay(300);
 
   // rotate 0 -> 180
-  for (int pos = 0; pos <= 180; pos++) {
+  for (int pos = 0; pos <= 180; pos++ {
     myServo.write(pos);
     delay(stepDelay);
   }
@@ -20,7 +20,7 @@ void setup() {
   for (int pos = 180; pos >= 0; pos --) {
     myServo.write(pos);
     delay(stepDelay);
-  }  
+  }
 }
 
 void loop() {
